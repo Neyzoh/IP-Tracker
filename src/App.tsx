71b76dfc1103bc596c6e945198b894ccf885b453
Adam Ammour <adamammour@neyzoh.home>
2023-03-31 16:01:33 +0200

    Start Navbar + setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import React from 'react'
+import Navbar from './components/Navbar'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <Navbar />
     </div>
   )
 }
